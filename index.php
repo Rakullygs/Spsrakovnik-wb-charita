053b8764757f06f9df2ea7c3da1b87a732c57511
horacekmila <horacekmila.1@seznam.cz>
2017-06-10 16:04:43 +0200

    init commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+      <head>
+        <title>Charita</title>
+               <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.css"> 
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/custom.css"
+     <link rel="stylesheet" href="source/font-awesome/css/font-awesome.min.css">
+    </head>
+    <body>
+        <?php
+        ?>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="header col-xs-12">
+                    <div class="row">      
+                        <div class="header-box-img col-xs-7">
+            
+                        </div>
+                        <div class="middle col-xs-4">
+                          <div class="row">
+                           <div class="middle-top col-xs-12"></div> 
+                           <div class="middle-midle offset-xs-1 col-xs-11"></div>
+                           <div class="middle-down col-xs-12">
+                               
+                           </div>
+                          </div> 
+                        </div>
+                        <div class="header-addjust col-xs-1">
+                    
+                        </div>
+                 </div>
+                </div>
+            </div>    
+            <div class="box">
+                
+            </div>
+       </div>
+    </body>
+</html>
