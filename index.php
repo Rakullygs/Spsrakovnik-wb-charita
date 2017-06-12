a9b9b4a144c8e791dd19bcbf99af3e94d207d18a
horacekmila <horacekmila.1@seznam.cz>
2017-06-12 17:44:41 +0200

    navigace-php-stránky-text

--- index.php
+++ index.php
@@ -36,13 +36,19 @@
                 </div>
             </div>
             <div class="main-box"> 
-             <div class="left-main-box offset-md-1 col-xs-12 col-md-5">
+             <div class="left-main-box col-xs-12 col-md-5">
                 <h2>Charita do Afriky dětem</h2><br>
 
 Postavili jsme v Africké Keni dětský domov pro děti s AIDS, popřípadě pro děti bez rodičů, které jim zemřeli na AIDS a také pro zneužívané Masajské holčičky, které byli provdány do manželství třeba již ve věku 4 let a byla jim některým provedena obřízka.K dnešku jsme jich zachránili již 44. Právě pro tyto děti stavíme a zozšiřujeme tento dětský domov a část výdělku z naší práce pro vás právě poputuje na onen projekt. Děkujeme vám.
              </div>
-                <div class="right-main-box">
+                <div class="right-main-box col-xs-12 col-md-5">
+                    <h2>ONE MORE DAY FOR CHILDREN</h2>
                     
+                    Náš nadační fond ONE MORE DAY FOR CHILDREN má krom 148 adoptivních rodičů na dálku také další plány. Rozšířit již postavený dětský domov na severu Keni v oblasti Laikipia poblíž města Doldol, kde k dnešku již žije 64 našich adoptivní děti, které již nikoho nemají.
+                    <br> <br>
+Dalšími plány jsou – Pomoci smutným případům dětí ze slumů poblíž města Nanyuki, které jsou buďto HIV pozitivní od narození, jelikož pozitivní krev jim předala matka, nebo jim rodiče zemřeli na AIDS a nemá se o ně kdo starat. Tudíž nemohou navštěvovat školu, strádají hlady, spí na jedné posteli bez matrace spolu s dalšími 5 sourozenci a mohl bych mluvit a psát ještě dlouho. Dále také masajské dívenky zachránit od manželství a pomoci jim formou adopce na dálku, díky které budou moci chodit do školy. Chceme pro ně sehnat adoptivní rodiče na dálku a umístit k nám do nově postaveného dětského domova.
+<br> <br>
+ZALOŽENÍ Nadačního fondu: Dne 3.4. 2009 jsem s pomocí Hellen Gathogo z Keni založil charitativní organizaci ve které působím od 16.8. 2010 jako president. V Keni s názvem „ONE MORE DAY FOR CHILDREN“ a v Čechách nadační fond se stejným názvem. Tato organizace pomáhá dětem v oblasti národního parku Mount Kenya , oblasti Laikipia a provincie Doldol v Keni.
                 </div>
             </div>    
       </div>      
