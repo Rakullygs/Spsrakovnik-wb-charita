2a57e3041b88bff925693130f2bbfff9fee23e41
Gamer <Gamer@10.0.0.34>
2017-06-13 13:43:00 +0200

    home-done kontakty-done

--- pages/contact_us.php
+++ pages/contact_us.php
@@ -1,13 +1,35 @@
 <head>
         <title>Charita</title>
-               <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+            <meta http-equiv="x-ua-compatible" content="ie=edge">
     <link rel="stylesheet" href="../css/bootstrap/css/bootstrap.css"> 
     <link rel="stylesheet" href="../css/bootstrap/css/bootstrap-theme.min.css">
     <link rel="stylesheet" href="../css/custom.css"
-     <link rel="stylesheet" href="../source/font-awesome/css/font-awesome.min.css">
-     <link rel="stylesheet" href="../source/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="../css/media.css"
+    <link rel="stylesheet" href="../source/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../source/font-awesome/css/font-awesome.css">
     </head>
  <?php require "logo.php"; ?>
  <?php require "nav.php"; ?>
+    <div class="col-md-4 vyrovnavac"></div>
+    <div class="lide">
+         <div class="left-contact col-xs-11 col-sm-12  col-md-4">
+             <p class="text-lide">  Petr Nový<br>
+             Hlavní ředitel<br>
+             +420 <?php echo rand(100,999) . "\n";echo rand(100,999) . "\n";echo rand(100,999);?><br>
+            </p>         
+         </div>
+         <div class="mid-contact col-xs-11 col-sm-12 col-md-4">
+             <p class="text-lide">  Jana Prášilová<br>
+             Hlavní vedoucí<br>
+             +420 <?php echo rand(100,999) . "\n";echo rand(100,999) . "\n";echo rand(100,999);?><br>
+             </p>
+        </div>
+        <div class="right-contact col-xs-11 col-sm-12 col-md-4 ">
+            <p class="text-lide">  Petr Pravý<br>
+             Vedoucí projekru<br>
+             +420 <?php echo rand(100,999) . "\n";echo rand(100,999) . "\n";echo rand(100,999);?><br>
+            </p> 
+        </div>    
+    </div>
--- pages/logo.php
+++ pages/logo.php
@@ -1,7 +1,7 @@
  <div class="header col-xs-12">
                     <div class="row">      
-                        <div class="header-box-img col-xs-7"></div>
-                        <div class="middle col-xs-4">
+                        <div class="header-box-img col-sm-7 col-xs-12"><h2 class="Naspis-phone hidden-lg-up">Charina na děti z Keni</h2> <small class="podtext-phone hidden-lg-up hidden-xs-down">pomocte těm co to potřebijí</small></div>
+                        <div class="middle col-xs-4 hidden-xs-down">
                           <div class="row">
                            <div class="middle-top col-xs-12"></div> 
                            <div class="middle-midle hidden-md-down offset-md-1 col-md-11"></div>
@@ -10,5 +10,4 @@
                          </div>
                       </div>
                      </div>    
-     
                 </div>
