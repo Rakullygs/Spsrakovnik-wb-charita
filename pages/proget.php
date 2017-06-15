dac842548fdf45141d257d82e1b5b4446e46c821
Gamer <Gamer@10.0.0.34>
2017-06-15 18:17:52 +0200

    hotovo

---
+++ pages/proget.php
@@ -0,0 +1,12 @@
+<head>
+        <title>Charita</title>
+               <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/bootstrap/css/bootstrap.css"> 
+    <link rel="stylesheet" href="../css/bootstrap/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="../css/custom.css"
+     <link rel="stylesheet" href="../source/font-awesome/css/font-awesome.min.css">
+    </head>
+ <?php require "logo.php"; ?>
+ <?php require "nav.php"; ?>
